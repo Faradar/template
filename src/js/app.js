f9ae401b238475cda273d813d1168f415bc4b369
Faradar <tomi_compuamste@hotmail.com>
2023-07-02 15:08:24 -0300

    Added html, js and some other stuff

---
+++ src/js/app.js
@@ -0,0 +1,2 @@
+const q = selector => document.querySelector(selector) // shortcut for document.querySelector
+const qa = selector => document.querySelectorAll(selector) // shortcut for document.querySelectorAll
